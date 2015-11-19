1957346b8b5690f9f891bb39e9e5dda7b3ccb24b
Glen Taylor <gtaylor37@gmail.com>
2015-11-19 11:28:39 -0500

    Organized items, modified random generator
    - Organized assets into respective folders
    - Random generator now adds only unique items to list and also generates
    a second list to use after first is depleted and in order to show next
    group once at list group in first sequence
    - Spawner includes new arts

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject[] groups;
+    //private int[] firstSelection;
+    //private int[] nextSelection;
+    public List<int> firstSelection;
+    public List<int> secondSelection;
+
+    public void spawnNext()
+    {
+        //int i = Random.Range(0, groups.Length);
+
+        //Instantiate(groups[i], transform.position, Quaternion.identity);
+        if (firstSelection.Count == 0)
+        {
+            firstSelection = new List<int>(secondSelection);
+            generateNextSequence();
+        }
+        int nextGroup = firstSelection[0];
+        firstSelection.RemoveAt(0);
+        Instantiate(groups[nextGroup], transform.position, Quaternion.identity);
+
+    }
+
+    public void spawnCollection()
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            int firstInt = Random.Range(0, groups.Length);
+            if (!firstSelection.Contains(firstInt)) firstSelection.Add(firstInt);
+            int secondInt = Random.Range(0, groups.Length);
+            if (!secondSelection.Contains(secondInt)) secondSelection.Add(secondInt);
+        }
+    }
+
+    public void generateNextSequence()
+    {
+        secondSelection.Clear();
+        for (int i = 0; i < 7; i++)
+        {
+            int secondInt = Random.Range(0, groups.Length);
+            if (!secondSelection.Contains(secondInt)) secondSelection.Add(secondInt);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        //spawnNext();
+        firstSelection = new List<int>();
+        secondSelection = new List<int>();
+        //nextSelection = new int[7];
+        spawnCollection();
+        spawnNext();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
