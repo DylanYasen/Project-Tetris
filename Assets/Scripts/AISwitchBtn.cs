b4ae8f00c4a03916b9f9f86d0bc35d1356cf5274
Yadikaer Yasheng <yadikae1@umbc.edu>
2015-11-30 16:28:21 -0500

    next piece & ai switch ui

---
+++ Assets/Scripts/AISwitchBtn.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AISwitchBtn : MonoBehaviour {
+	
+	bool isAiMode = true;
+	private Text text;
+	private string playerModeText = "P";
+	private string aiModeText = "AI";
+
+	void Start () {
+		Group.aimode = isAiMode;	
+		
+		text = transform.GetChild(0).GetComponent<Text>();
+	}
+	
+	public void Switch(){
+		isAiMode = !isAiMode;
+		Group.aimode = isAiMode;	
+		
+		if(Group.aimode == true)
+			text.text = aiModeText;
+		else
+			text.text = playerModeText;
+	}
+}
